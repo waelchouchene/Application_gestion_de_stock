d34393f350f4f1362b43cd3e707faffe76358d31
Wael <etudiant.chouchene.wael@uvt.tn>
2024-02-11 17:07:02 +0100

    herocu change

--- gestionlivraisons/models.py
+++ gestionlivraisons/models.py
@@ -10,9 +10,9 @@ class Livraison(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField()
     montant_total = models.FloatField()
-    commande = models.ForeignKey(Commande, models.DO_NOTHING)
-    vehicule = models.ForeignKey(Vehicule, models.DO_NOTHING)
-    chauffeur = models.ForeignKey(Personnel, models.DO_NOTHING)
+    commande = models.ForeignKey(Commande, models.DO_NOTHING,null=True)
+    vehicule = models.ForeignKey(Vehicule, models.DO_NOTHING,null=True)
+    chauffeur = models.ForeignKey(Personnel, models.DO_NOTHING,null=True)
 
     def __str__(self):
             return self.commande.libelle
--- gestionstocks/settings.py
+++ gestionstocks/settings.py
@@ -96,14 +96,24 @@ WSGI_APPLICATION = 'gestionstocks.wsgi.application'
 #   }
 #}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Base de données postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_stock',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
