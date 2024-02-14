5762a73fcf5f22781fd8189e771c20d13e05d728
Wael <etudiant.chouchene.wael@uvt.tn>
2024-02-14 20:29:02 +0100

    'render'

--- gestionstocks/settings.py
+++ gestionstocks/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import environ
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -104,17 +105,21 @@ WSGI_APPLICATION = 'gestionstocks.wsgi.application'
 # }
 
 # Base de données postgresql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_stock',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_stock',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    {
+        'default': dj_database_url.parse(env('DATABASE_URL'))
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -152,4 +157,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+]
+MEDIA_URL='images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'images/')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
